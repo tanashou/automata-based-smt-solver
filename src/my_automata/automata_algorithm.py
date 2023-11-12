a038a9ea2ce5a097bb86721faff509f52a8eb275
tanashou <tanashou0210@gmail.com>
2023-11-12 19:43:13 +0900

    add nfa_intersection

--- src/my_automata/automata_algorithm.py
+++ src/my_automata/automata_algorithm.py
@@ -1,13 +1,14 @@
 from itertools import product
 from src.my_automata.my_automata import MutableNFA as NFA
+from typing import List
 
 
 # TODO: ワイルドカードを使用できるようにする
-def binary_strings(n):
+def binary_strings(n) -> set[str]:
     # Generate all possible combinations of 0 and 1 of length n
     combinations = product("01", repeat=n)
     # Join the tuples to form binary strings
-    return ["".join(x) for x in combinations]
+    return set("".join(x) for x in combinations)
 
 
 def dot_product(vector1, vector2):
@@ -27,7 +28,7 @@ c: constant of the linear equation
 """
 
 
-def eq_to_nfa(a, c: int):
+def eq_to_nfa(a: List[int], c: int):
     initial_state = "q0"
     states = {initial_state, str(c)}
     input_symbols = binary_strings(len(a))
@@ -49,5 +50,39 @@ def eq_to_nfa(a, c: int):
             if current_state == -dot:
                 nfa.add_transition(initial_state, symbol, str(current_state))
                 # return nfa # TODO: オートマトンを完全に作るかどうか
+
+    return nfa
+
+
+def neq_to_nfa(a: List[int], c: int):
+    pass
+
+
+# wildcard を使用するため、自作する必要がある
+def nfa_intersection(nfa1: NFA, nfa2: NFA):
+    states = set()
+    input_symbols = nfa1.input_symbols.intersection(nfa2.input_symbols)
+    transitions = dict()
+    initial_state = (nfa1.initial_state, nfa2.initial_state)
+    final_states = set()
+    work_list = [initial_state]
+
+    nfa = NFA(states, input_symbols, transitions, initial_state, final_states)
+
+    if not input_symbols:
+        raise ValueError("The given NFAs have no common input symbols")
+
+    while work_list:
+        current_state1, current_state2 = work_list.pop()
+        nfa.add_state((current_state1, current_state2))
+        if current_state1 in nfa1.final_states and current_state2 in nfa2.final_states:
+            nfa.add_final_state((current_state1, current_state2))
+        for symbol in input_symbols:
+            next_states1 = nfa1.find_transitions_from_keys(current_state1, symbol)
+            next_states2 = nfa2.find_transitions_from_keys(current_state2, symbol)
+            for next_state1, next_state2 in set(product(next_states1, next_states2)):
+                nfa.add_transition((current_state1, current_state2), symbol, (next_state1, next_state2))
+                if (next_state1, next_state2) not in states:
+                    work_list.append((next_state1, next_state2))
 
     return nfa
--- src/playground.py
+++ src/playground.py
@@ -27,5 +27,8 @@ nfa2 = NFA(
     final_states={"q1"},
 )
 
-test_nfa = eq_to_nfa([1, -1], 2)
-test_nfa.show_diagram(path="test_nfa.png")
+test_nfa1 = eq_to_nfa([1, -2], 3)
+test_nfa2 = eq_to_nfa([1, -1], 2)
+nfa3 = nfa_intersection(test_nfa1, test_nfa2)
+nfa3.show_diagram(path='intersection.png')
+
