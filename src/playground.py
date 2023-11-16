c9472a05d76daa1453653699bd3fa42f19ccd08e
tanashou <tanashou0210@gmail.com>
2023-11-16 15:24:11 +0900

    Add AutomataConverter class for creating NFAs from linear equations

--- src/playground.py
+++ src/playground.py
@@ -31,13 +31,13 @@ coefs = [1, -1, 1]
 mask1 = [True, True, False]
 mask2 = [True, False, True]
 
-complete_nfa1 = eq_to_nfa(coefs, 2, mask1)
-complete_nfa2 = eq_to_nfa(coefs, 5, mask2)
-partial_nfa1 = eq_to_nfa(coefs, 2, mask1, create_all=False)
-partial_nfa2 = eq_to_nfa(coefs, 5, mask2, create_all=False)
+complete_nfa1 = AutomataConverter(coefs, 2, 'equal', mask1)
+complete_nfa2 = AutomataConverter(coefs, 5, 'equal', mask2)
+partial_nfa1 = AutomataConverter(coefs, 2, 'equal', mask1, create_all=False)
+partial_nfa2 = AutomataConverter(coefs, 5, 'equal', mask2, create_all=False)
 
 
-complete_nfa = nfa_intersection(complete_nfa1, complete_nfa2, mask1, mask2)
-partial_nfa = nfa_intersection(partial_nfa1, partial_nfa2, mask1, mask2)
+complete_nfa = nfa_intersection(complete_nfa1.nfa, complete_nfa2.nfa, mask1, mask2)
+partial_nfa = nfa_intersection(partial_nfa1.nfa, partial_nfa2.nfa, mask1, mask2)
 complete_nfa.show_diagram(path="image/complete1.png")
 partial_nfa.show_diagram(path="image/partial1.png")
