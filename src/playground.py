70756260b57b6c8bc737d26176bd2f828aa3db65
tanashou <tanashou0210@gmail.com>
2023-11-17 16:40:23 +0900

    Refactor AutomataBuilder class to improve performance and fix bugs

--- src/playground.py
+++ src/playground.py
@@ -31,13 +31,23 @@ coefs = [1, -1, 1]
 mask1 = [True, True, False]
 mask2 = [True, False, True]
 
-complete_nfa1 = AutomataConverter(coefs, 2, 'equal', mask1)
-complete_nfa2 = AutomataConverter(coefs, 5, 'equal', mask2)
-partial_nfa1 = AutomataConverter(coefs, 2, 'equal', mask1, create_all=False)
-partial_nfa2 = AutomataConverter(coefs, 5, 'equal', mask2, create_all=False)
+# complete_nfa1 = AutomataBuilder(coefs, 2, 'equal', mask1, create_all=True)
+# complete_nfa2 = AutomataBuilder(coefs, 5, 'equal', mask2, create_all=True)
+partial_nfa1 = AutomataBuilder(coefs, 2, 'equal', mask1, create_all=False)
+partial_nfa2 = AutomataBuilder(coefs, 5, 'equal', mask2, create_all=False)
+
+# complete_nfa1.next()
+# complete_nfa2.next()
+# partial_nfa1.next()
+# partial_nfa2.next()
+
+partial_nfa1.next()
+partial_nfa1.nfa.show_diagram(path="image/partial1.png")
+partial_nfa1.next()
+partial_nfa1.nfa.show_diagram(path="image/partial2.png")
 
 
-complete_nfa = nfa_intersection(complete_nfa1.nfa, complete_nfa2.nfa, mask1, mask2)
-partial_nfa = nfa_intersection(partial_nfa1.nfa, partial_nfa2.nfa, mask1, mask2)
-complete_nfa.show_diagram(path="image/complete1.png")
-partial_nfa.show_diagram(path="image/partial1.png")
+# # complete_nfa = nfa_intersection(complete_nfa1.nfa, complete_nfa2.nfa, mask1, mask2)
+# partial_nfa = nfa_intersection(partial_nfa1.nfa, partial_nfa2.nfa, mask1, mask2)
+# # complete_nfa.show_diagram(path="image/complete1.png")
+# partial_nfa.show_diagram(path="image/partial1.png")
