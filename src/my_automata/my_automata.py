86f2c21fea739f6703be785648b96d6489400bc7
tanashou <tanashou0210@gmail.com>
2023-11-18 15:47:51 +0900

    Refactor MutableNFA class to use type hints and improve code readability

--- src/my_automata/my_automata.py
+++ src/my_automata/my_automata.py
@@ -1,24 +1,28 @@
 from automata.fa.nfa import NFA as BaseNFA
-import automata.fa.nfa as nfa
 from collections import defaultdict
 
 
 """
-The instance variables, such as 'states', in the NFA class from automata-lib are immutable by default.
+The instance variables, such as 'states', in the NFA class from automata-lib are immutable.
 I needed a mutable version of these variables to modify them during runtime, so I created a separate mutable object.
 """
 
+SymbolT = str
+NFAStateT = str
+NFAPathT = defaultdict[str, set[NFAStateT]]
+NFATransitionT = defaultdict[NFAStateT, NFAPathT]
+InputPathListT = list[tuple[NFAStateT, NFAStateT, SymbolT]]
 
-# TODO: 作成したオートマトンが目的のものでなかった場合、再度途中から作り直す。この時、止まった時の変数の状態を保存しておく必要がある。ループ変数やwork_listの状態など
+
 class MutableNFA:
     def __init__(
         self,
         *,
-        states: set[nfa.NFAStateT],
-        input_symbols: set[str],  # TODO: 全てのアルファベットを保存しておく必要はないので、正規表現などにしたい
-        transitions: "defaultdict(nfa.NFAStateT, nfa.NFAPathT)",
-        initial_state: nfa.NFAStateT,
-        final_states: set[nfa.NFAStateT],
+        states: set[NFAStateT],
+        input_symbols: set[SymbolT],  # TODO: 全てのアルファベットを保存しておく必要はないので、正規表現などにしたい
+        transitions: NFATransitionT,
+        initial_state: NFAStateT,
+        final_states: set[NFAStateT],
     ) -> None:
         """Initialize a complete NFA."""
 
@@ -28,29 +32,28 @@ class MutableNFA:
         self.initial_state = initial_state
         self.final_states = final_states
 
-    def add_state(self, new_state: nfa.NFAStateT):
+    def add_state(self, new_state: NFAStateT) -> None:
         self.states.add(new_state)
 
-    def add_states(self, new_states: set[nfa.NFAStateT]):
+    def add_states(self, new_states: set[NFAStateT]) -> None:
         self.states.update(new_states)
 
-    def add_input_symbol(self, new_input_symbol: str):
+    def add_input_symbol(self, new_input_symbol: str) -> None:
         self.input_symbols.add(new_input_symbol)
 
-    def add_transition(self, current_state: nfa.NFAStateT, symbol: str, next_state: nfa.NFAStateT):
+    def add_transition(self, current_state: NFAStateT, symbol: str, next_state: NFAStateT) -> None:
         self.transitions[current_state][symbol].add(next_state)
 
-    def add_initial_state(self, new_initial_state: nfa.NFAStateT):
+    def add_initial_state(self, new_initial_state: NFAStateT) -> None:
         self.initial_state = new_initial_state
 
-    def add_final_state(self, new_final_state: nfa.NFAStateT):
+    def add_final_state(self, new_final_state: NFAStateT) -> None:
         self.final_states.add(new_final_state)
 
-    # find a state that has the same transitions as the given state
-    def find_transitions_from_keys(self, current_state: nfa.NFAStateT, symbol: str) -> set[nfa.NFAStateT]:
-        return self.transitions.get(current_state, {}).get(symbol, set())
+    def find_transitions_from_keys(self, current_state: NFAStateT, symbol: SymbolT) -> set[NFAStateT]:
+        return self.transitions[current_state][symbol]
 
-    def __make_base_nfa(self):
+    def __make_base_nfa(self) -> None:
         self.__base_nfa = BaseNFA(
             states=self.states,
             input_symbols=self.input_symbols,
@@ -59,6 +62,6 @@ class MutableNFA:
             final_states=self.final_states,
         )
 
-    def show_diagram(self, path=None):
+    def show_diagram(self, path: str) -> None:
         self.__make_base_nfa()
         self.__base_nfa.show_diagram(path=path)
