126e6305fd77408181463e15836249f472e004c3
tanashou <tanashou0210@gmail.com>
2023-11-30 17:15:19 +0900

    Add pytest to dependencies

--- src/my_automata/automata_algorithms.py
+++ src/my_automata/automata_algorithms.py
@@ -1,7 +1,7 @@
 import itertools
 from collections import defaultdict
-from my_automata.mutable_nfa import MutableNFA as NFA, SymbolT, NFAStateT
-from my_automata.utils import (
+from src.my_automata.mutable_nfa import MutableNFA as NFA, SymbolT, NFAStateT
+from src.my_automata.utils import (
     binary_strings_with_wildcard,
     dot_product_with_wildcard,
     apply_mask,
--- src/test/test_util.py
+++ src/test/test_util.py
@@ -1,57 +1,54 @@
-import unittest
-# need path start from src. otherwise, won't work
+# need path from 'src'. otherwise, pytest cannot find the module
 from src.my_automata.utils import binary_strings_with_wildcard, dot_product_with_wildcard
+import pytest
 
 
 # Test cases for the function binary_strings_with_wildcard
-class TestBinaryStringsWithWildcard(unittest.TestCase):
-    def test_no_wildcards(self) -> None:
+class TestBinaryStringsWithWildcard:
+    def test_no_wildcards(self):
         mask = [True, True, True]
         expected_output = {"000", "001", "010", "011", "100", "101", "110", "111"}
-        self.assertEqual(binary_strings_with_wildcard(mask), expected_output)
+        assert binary_strings_with_wildcard(mask) == expected_output
 
-    def test_all_wildcards(self) -> None:
+    def test_all_wildcards(self):
         mask = [False, False, False]
         expected_output = {"***"}
-        self.assertEqual(binary_strings_with_wildcard(mask), expected_output)
+        assert binary_strings_with_wildcard(mask) == expected_output
 
-    def test_mixed_mask(self) -> None:
+    def test_mixed_mask(self):
         mask = [True, False, True]
         expected_output = {"0*0", "0*1", "1*0", "1*1"}
-        self.assertEqual(binary_strings_with_wildcard(mask), expected_output)
+        assert binary_strings_with_wildcard(mask) == expected_output
 
-    def test_single_true_mask(self) -> None:
+    def test_single_true_mask(self):
         mask = [True]
         expected_output = {"0", "1"}
-        self.assertEqual(binary_strings_with_wildcard(mask), expected_output)
+        assert binary_strings_with_wildcard(mask) == expected_output
 
-    def test_single_false_mask(self) -> None:
+    def test_single_false_mask(self):
         mask = [False]
         expected_output = {"*"}
-        self.assertEqual(binary_strings_with_wildcard(mask), expected_output)
+        assert binary_strings_with_wildcard(mask) == expected_output
 
 
-class TestDotProductWithWildcard(unittest.TestCase):
-    def test_all_values_no_wildcard(self) -> None:
-        self.assertEqual(dot_product_with_wildcard(["1", "0", "1"], "101"), 2)
+# Test cases for the function dot_product_with_wildcard
+class TestDotProductWithWildcard:
+    def test_all_values_no_wildcard(self):
+        assert dot_product_with_wildcard(["1", "0", "1"], "101") == 2
 
-    def test_with_wildcards(self) -> None:
-        self.assertEqual(dot_product_with_wildcard(["1", "1", "1"], "1*1"), 2)
+    def test_with_wildcards(self):
+        assert dot_product_with_wildcard(["1", "1", "1"], "1*1") == 2
 
-    def test_all_wildcards(self) -> None:
-        self.assertEqual(dot_product_with_wildcard(["1", "1", "1"], "***"), 0)
+    def test_all_wildcards(self):
+        assert dot_product_with_wildcard(["1", "1", "1"], "***") == 0
 
-    def test_empty_lists(self) -> None:
-        self.assertEqual(dot_product_with_wildcard([], ""), 0)
+    def test_empty_lists(self):
+        assert dot_product_with_wildcard([], "") == 0
 
-    def test_mismatched_length(self) -> None:
-        with self.assertRaises(ValueError):
+    def test_mismatched_length(self):
+        with pytest.raises(ValueError):
             dot_product_with_wildcard(["1", "0"], "101")
 
-    def test_invalid_input(self) -> None:
-        with self.assertRaises(ValueError):
+    def test_invalid_input(self):
+        with pytest.raises(ValueError):
             dot_product_with_wildcard(["1", "a", "1"], "101")
-
-
-if __name__ == "__main__":
-    unittest.main()
