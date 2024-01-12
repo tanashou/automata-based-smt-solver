c22380489abd5fdd7fd612c51b5a97f838822e75
tanashou <tanashou0210@gmail.com>
2024-01-12 16:31:21 +0900

    Add PresburgerArithmeticExpression class to handle arithmetic expressions

---
+++ src/my_smt_solver/presburger_arithmetic_expression.py
@@ -0,0 +1,24 @@
+from .type import Relation
+
+
+class PresburgerArithmeticExpression:
+    def __init__(self, terms: list[tuple[str, int]], relation: Relation, const: int) -> None:
+        self.__terms = terms
+        self.__relation = relation
+        self.__const = const
+
+    @property
+    def terms(self) -> list[tuple[str, int]]:
+        return self.__terms
+
+    @terms.setter
+    def terms(self, terms: list[tuple[str, int]]) -> None:
+        self.__terms = terms
+
+    @property
+    def relation(self) -> Relation:
+        return self.__relation
+
+    @property
+    def const(self) -> int:
+        return self.__const
