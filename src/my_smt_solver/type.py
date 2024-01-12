7174bc7c60b4acec045f713681f4579f0eb2637f
tanashou <tanashou0210@gmail.com>
2024-01-12 16:25:21 +0900

    manually add relation from relation-enum branch

--- src/my_smt_solver/type.py
+++ src/my_smt_solver/type.py
@@ -1,8 +1,25 @@
 from collections import defaultdict
 from typing import Any
+from enum import Enum
 
 SymbolT = str
 NFAStateT = Any  # 入れ子になる可能性があるので、Anyにしておく
 NFAPathT = defaultdict[SymbolT, set[NFAStateT]]
 NFATransitionT = defaultdict[NFAStateT, NFAPathT]
 InputPathListT = list[tuple[NFAStateT, NFAStateT, SymbolT]]
+
+
+class Relation(Enum):
+    EQ = "="  # Equals
+    NEQ = "!="  # Not Equals
+    LT = "<"  # Less Than
+    GT = ">"  # Greater Than
+    LEQ = "<="  # Less than or Equal to
+    GEQ = ">="  # Greater than or Equal to
+
+    @classmethod
+    def from_str(cls, relation_str: str) -> "Relation":
+        for relation in cls:
+            if relation.value == relation_str:
+                return relation
+        raise ValueError(f"Invalid relation: {relation_str}")
