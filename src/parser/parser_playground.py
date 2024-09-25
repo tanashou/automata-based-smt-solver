c170ea37d59359236e5d2e843c3e0b98cbbcb15a
tanashou <tanashou0210@gmail.com>
2024-09-25 11:17:41 +0000

    fix to use logger

--- src/parser/parser_playground.py
+++ src/parser/parser_playground.py
@@ -1,5 +1,7 @@
+import logging
 import sys
 import time
+from functools import wraps
 
 from antlr4 import *
 
@@ -7,28 +9,57 @@ from parser.antlr.SMTLIBv2Lexer import SMTLIBv2Lexer
 from parser.antlr.SMTLIBv2Parser import SMTLIBv2Parser
 from parser.visitors.CustomVisitor import CustomVisitor
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+# Decorator to time a function
+def timer(section_name):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            start_time = time.time()
+            result = func(*args, **kwargs)
+            end_time = time.time()
+            logger.info(f"{section_name} time: {end_time - start_time}")
+            return result
+
+        return wrapper
+
+    return decorator
+
 
 def main(argv):
     input_stream = FileStream(argv[1])
-    start_time = time.time()
-    lexer = SMTLIBv2Lexer(input_stream)
-    read_time = time.time()
-    print(f"Read time: {read_time - start_time}")
-    stream = CommonTokenStream(lexer)
-    stream_time = time.time()
-    print(f"Stream time: {stream_time - read_time}")
-    parser = SMTLIBv2Parser(stream)
-    end_time = time.time()
-    print(f"Parser time: {end_time - stream_time}")
 
-    tree = parser.start()
-    tree_time = time.time()
-    print(f"Tree time: {tree_time - end_time}")
-    visitor = CustomVisitor()
-    result = visitor.visit(tree)
+    @timer("Lexer")
+    def create_lexer(input_stream):
+        return SMTLIBv2Lexer(input_stream)
+
+    @timer("Stream")
+    def create_stream(lexer):
+        return CommonTokenStream(lexer)
+
+    @timer("Parser")
+    def create_parser(stream):
+        return SMTLIBv2Parser(stream)
+
+    @timer("Tree")
+    def parse_tree(parser):
+        return parser.start()
+
+    @timer("Visitor")
+    def visit_tree(tree):
+        visitor = CustomVisitor()
+        return visitor.visit(tree)
+
+    lexer = create_lexer(input_stream)
+    stream = create_stream(lexer)
+    parser = create_parser(stream)
+    tree = parse_tree(parser)
+    result = visit_tree(tree)
+
     print(result)
-    visitor_time = time.time()
-    print(f"Visitor time: {visitor_time - tree_time}")
 
 
 if __name__ == "__main__":
