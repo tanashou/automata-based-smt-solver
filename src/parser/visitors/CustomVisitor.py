5c3b930a1e523de52b0ed5d11ffe86dd1794c316
tanashou <tanashou0210@gmail.com>
2024-09-27 03:22:47 +0000

    fix visit function

--- src/parser/visitors/CustomVisitor.py
+++ src/parser/visitors/CustomVisitor.py
@@ -16,36 +16,30 @@ class CustomVisitor(SMTLIBv2Visitor):
         # visitor.visit(tree) の戻り値になる
         return self._result
 
-    def visitCommand(self, ctx: SMTLIBv2Parser.CommandContext):
-        if ctx.cmd_declareFun():
-            # 同じ command 内に複数の declare-fun が存在しないので0を指定
-            symbol_ctx = ctx.symbol(0)
-            variable_name = self.visitSymbol(symbol_ctx)
-            sort_ctxs = ctx.sort()
-            # declareFun で定義される関数は戻り値が1つのみ。
-            # 最後が戻り値の型なので、それ以外を引数とする。
-            *fun_args, fun_return_type = [
-                self.visitSort(sort_ctx) for sort_ctx in sort_ctxs
-            ]
+    def visitCmd_declareFun(self, ctx: SMTLIBv2Parser.Cmd_declareFunContext):
+        command_ctx = ctx.parentCtx
+        symbol_ctx = command_ctx.symbol(0)
+        variable_name = self.visitSymbol(symbol_ctx)
+        sort_ctxs = command_ctx.sort()
+        # declareFun で定義される関数は戻り値が1つのみ
+        # 最後が戻り値の型なので、それ以外を引数とする
+        *fun_arg_types, fun_return_type = [
+            self.visitSort(sort_ctx) for sort_ctx in sort_ctxs
+        ]
+        logger.info(
+            "Declare function: %s (%s) -> %s",
+            variable_name,
+            fun_arg_types,
+            fun_return_type,
+        )
+        self._result.append(variable_name)
 
-            logger.info(
-                "Declare function: %s (%s) -> %s",
-                variable_name,
-                fun_args,
-                fun_return_type,
-            )
-            # TODO: 変数名、引数、戻り値を結果に入れたい
-            self._result.append(variable_name)
-
-        elif ctx.cmd_setLogic():
-            # 同じ command 内に複数の set-logic が存在しないので0を指定
-            symbol = ctx.symbol(0)
-            logic_name = self.visitSymbol(symbol)
-            if logic_name:
-                logger.info("Set logic: %s", logic_name)
-                self._result.append(logic_name)
-                return
-        return  # これ以上探索する必要がない
+    def visitCmd_setLogic(self, ctx: SMTLIBv2Parser.Cmd_setLogicContext):
+        command_ctx = ctx.parentCtx
+        symbol = command_ctx.symbol(0)
+        logic_name = self.visitSymbol(symbol)
+        logger.info("Set logic: %s", logic_name)
+        self._result.append(logic_name)
 
     def visitSymbol(self, ctx: SMTLIBv2Parser.SymbolContext):
         # Handle simpleSymbol and quotedSymbol
@@ -66,6 +60,4 @@ class CustomVisitor(SMTLIBv2Visitor):
         return rslt
 
     def visitQuotedSymbol(self, ctx: SMTLIBv2Parser.QuotedSymbolContext):
-        # Return the text of the quoted symbol, stripping the surrounding '|'
-        text = ctx.getText()
-        return text[1:-1]  # Remove the leading and trailing '|'
+        return ctx.getText()
