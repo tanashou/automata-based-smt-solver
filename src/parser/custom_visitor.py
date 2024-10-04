794c1b86e7a186d563049ade40071c50610e5189
tanashou <tanashou0210@gmail.com>
2024-10-04 05:32:39 +0000

    refactor code

--- src/parser/custom_visitor.py
+++ src/parser/custom_visitor.py
@@ -45,76 +45,30 @@ class CustomVisitor(SMTLIBv2Visitor):
         logger.info("Set logic: %s", logic_name)
         self._result.append(logic_name)
 
-    def visitTerm(self, ctx: SMTLIBv2Parser.TermContext):
+    def visitTerm(self, ctx: SMTLIBv2Parser.TermContext) -> SMTLIBv2Term:
+        # Handle spec_constant or qual_identifier without terms
         if not ctx.term():
-            # spec_constant or qual_identifier
             if ctx.spec_constant():
                 spec_constant = self.visitSpec_constant(ctx.spec_constant())
                 return SMTLIBv2Term(spec_constant=spec_constant)
+
             if ctx.qual_identifier():
-                qual_ideitifier = self.visitQual_identifier(ctx.qual_identifier())
-                return qual_ideitifier
-        elif ctx.qual_identifier():
-            # Handle (qual_identifier term+)
-            qual_ideitifier = self.visitQual_identifier(ctx.qual_identifier())
+                qual_identifier = self.visitQual_identifier(ctx.qual_identifier())
+                return SMTLIBv2Term(qual_identifier=qual_identifier)
+
+        # Handle (qual_identifier term+)
+        if ctx.qual_identifier():
+            qual_identifier = self.visitQual_identifier(ctx.qual_identifier())
             terms = [self.visitTerm(term) for term in ctx.term()]
-            return (qual_ideitifier, terms)
-        # QF_LIA では上3つだけ使用されるはず
-        elif ctx.GRW_Let():
-            # Handle (let (var_binding+) term)
-            var_bindings = [
-                self.visitVar_binding(binding) for binding in ctx.var_binding()
-            ]
-            term = self.visitTerm(ctx.term(0))
-            logger.info("let with bindings: %s and term: %s", var_bindings, term)
-            return (SMTLIBv2Type.GRW_Let, var_bindings, term)
-        elif ctx.GRW_Forall():
-            # Handle (forall (sorted_var+) term)
-            sorted_vars = [self.visitSorted_var(var) for var in ctx.sorted_var()]
-            term = self.visitTerm(ctx.term(0))
-            logger.info("forall with sorted vars: %s and term: %s", sorted_vars, term)
-            return (SMTLIBv2Type.GRW_Forall, sorted_vars, term)
-        elif ctx.GRW_Exists():
-            # Handle (exists (sorted_var+) term)
-            sorted_vars = [self.visitSorted_var(var) for var in ctx.sorted_var()]
-            term = self.visitTerm(ctx.term(0))
-            logger.info("exists with sorted vars: %s and term: %s", sorted_vars, term)
-            return (SMTLIBv2Type.GRW_Exists, sorted_vars, term)
-        elif ctx.GRW_Match():
-            # Handle (match term (match_case+))
-            match_term = self.visitTerm(ctx.term(0))
-            match_cases = [self.visitMatch_case(case) for case in ctx.match_case()]
-            logger.info("match with term: %s and cases: %s", match_term, match_cases)
-            return (SMTLIBv2Type.GRW_Match, match_term, match_cases)
-        elif ctx.GRW_Exclamation():
-            # Handle (! term attribute+)
-            exclam_term = self.visitTerm(ctx.term(0))
-            attributes = [self.visitAttribute(attr) for attr in ctx.attribute()]
-            logger.info(
-                "exclamation with term: %s and attributes: %s", exclam_term, attributes
-            )
-            return (SMTLIBv2Type.GRW_Exclamation, exclam_term, attributes)
-        else:
-            # ここには到達しないはず
-            raise NotImplementedError("Unknown term")
-        return None
+            return SMTLIBv2Term(qual_identifier=qual_identifier, terms=terms)
 
-    def visitVar_binding(self, ctx: SMTLIBv2Parser.Var_bindingContext):
-        pass
-
-    def visitSorted_var(self, ctx: SMTLIBv2Parser.Sorted_varContext):
-        return super().visitSorted_var(ctx)
-
-    def visitMatch_case(self, ctx: SMTLIBv2Parser.Match_caseContext):
-        return super().visitMatch_case(ctx)
-
-    def visitAttribute(self, ctx: SMTLIBv2Parser.AttributeContext):
-        return super().visitAttribute(ctx)
+        # Raise an error for unexpected cases
+        mssg = "Unknown term encountered in visitTerm"
+        raise NotImplementedError(mssg)
 
     def visitSpec_constant(
         self, ctx: SMTLIBv2Parser.Spec_constantContext
     ) -> SpecConstant:
-        # List of method references and their corresponding names
         type_conversion_map = [
             (ctx.numeral, SMTLIBv2Type.Numeral, int),
             (ctx.decimal, SMTLIBv2Type.Decimal, float),
