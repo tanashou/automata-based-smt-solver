11331386ba6cdebbbd58de4df172c9777e5b8506
tanashou <tanashou0210@gmail.com>
2024-10-22 09:06:52 +0000

    refactor code

--- src/parser/playground.py
+++ src/parser/playground.py
@@ -1,23 +1,10 @@
-from pysmt.smtlib.parser import SmtLibParser
-
 from parser.smt_to_sympy import SMTToSymPy
-
-parser = SmtLibParser()
-
 
 smt_file_path = "benchmarks/QF_LIA/prime-cone/prime_cone_sat_2.smt2"
 
-# TODO: sat, unsat, unknown を定数として扱う。enum で定義する。
-# expected_status = None
-# for cmd in script.commands:
-#     if cmd.name == "set-info" and cmd.args[0] == ":status":
-#         expected_status = cmd.args[1]
-#         break
 
-
-smt2sympy = SMTToSymPy()
-smt2sympy.set_smt_script(smt_file_path, is_file=True)
-sympy_expr_dnf = smt2sympy.get_rearranged_sympy_expression_as_dnf()
+smt2sympy = SMTToSymPy(source=smt_file_path, is_file=True)
+sympy_expr_dnf = smt2sympy.get_sympy_expression_as_dnf()
 print(sympy_expr_dnf)
 print(smt2sympy.sat_status)
 
