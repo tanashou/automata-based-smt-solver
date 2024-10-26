cd4b821fd554fc4ed28f517c9e670e28ae458186
tanashou <tanashou0210@gmail.com>
2024-10-26 08:18:03 +0000

    add

--- src/parser/playground.py
+++ src/parser/playground.py
@@ -5,6 +5,7 @@ smt_file_path = "benchmarks/QF_LIA/prime-cone/prime_cone_sat_2.smt2"
 
 smt2sympy = SMTToSymPy(source=smt_file_path, is_file=True)
 sympy_expr_dnf = smt2sympy.get_sympy_expression_as_dnf()
+print(smt2sympy.declared_vars)
 print(sympy_expr_dnf)
 print(smt2sympy.sat_status)
 
