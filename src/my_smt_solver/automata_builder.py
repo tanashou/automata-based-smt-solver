88e18d7a067d394d969ab410cbac3e0e080d9876
tanashou <tanashou0210@gmail.com>
2024-10-26 08:17:24 +0000

    refactor to use fixed functions

--- src/my_smt_solver/automata_builder.py
+++ src/my_smt_solver/automata_builder.py
@@ -1,8 +1,8 @@
 from collections import defaultdict
 
+from sympy import Eq, Ge, Gt, Le, Lt, Ne
+
 from .nfa import NFA
-from .presburger_arithmetic import PresburgerArithmetic
-from .type import Relation
 from .utils import (
     dot_product_with_wildcard,
     make_binary_wildcard_strings,
@@ -15,20 +15,25 @@ class AutomataBuilder:
 
     def __init__(
         self,
-        coefs: list[int],
-        prb_arithmetic: PresburgerArithmetic,
+        formula: Eq | Le | Ne | Ge | Gt | Lt,
+        declared_vars_index_map: dict[str, int],
         create_all: bool = False,
     ) -> None:
-        self.coefs = coefs
-        self.const = prb_arithmetic.const
-        self.relation = prb_arithmetic.relation
-        self.create_all = create_all  # for debug
+        # lhs, rhs は sympy.core や sympy.numbersとなる。
+        # リンターはBasicとして認識するので無視する
+        self.coefs: defaultdict[str, int] = formula.lhs.as_coefficients_dict()  # type: ignore[attr-defined]
+        self.const: int = int(formula.rhs)  # type: ignore[attr-defined]
+        self.relation: str = formula.rel_op
+        self.declared_vars_index_map: dict[str, int] = declared_vars_index_map
+        self.create_all: bool = create_all  # for debug
         self.nfa = NFA(
             states={self.INITIAL_STATE, str(self.const)},
-            input_symbols=make_binary_wildcard_strings(self.coefs),
+            input_symbols=make_binary_wildcard_strings(
+                declared_vars_index_map, self.coefs
+            ),
             transitions=defaultdict(lambda: defaultdict(set), {}),
             initial_state=self.INITIAL_STATE,
-            final_states=set([str(self.const)]),
+            final_states={str(self.const)},
         )
         self.work_list = [self.const]
         self.__build_completed = False
@@ -38,18 +43,14 @@ class AutomataBuilder:
         return self.__build_completed
 
     def next(self) -> None:
+        # FIXME: self.relation が str になったので、それの対応
         match self.relation:
-            case Relation.EQ:
+            case "==":
                 self.eq_to_nfa()
-            case Relation.NEQ:
-                self.neq_to_nfa()
-            case Relation.LEQ:
+            case "!=":
+                raise NotImplementedError("NEQ is not implemented yet")
+            case "<=":
                 self.leq_to_nfa()
-
-    """
-    coefs: all the coefficients of the linear equations
-    const: constant of the linear equation
-    """
 
     def eq_to_nfa(self) -> None:
         partial_sat = False
@@ -57,8 +58,11 @@ class AutomataBuilder:
         while self.work_list:
             current_state = self.work_list.pop()
             for symbol in self.nfa.input_symbols:
-                dot = dot_product_with_wildcard(self.coefs, symbol)
-                if (previous_state := 0.5 * (current_state - dot)).is_integer():
+                dot = dot_product_with_wildcard(
+                    self.declared_vars_index_map, self.coefs, symbol
+                )
+                if (current_state - dot) & 1 == 0:
+                    previous_state = (current_state - dot) // 2
                     previous_state = int(previous_state)
                     if str(previous_state) not in self.nfa.states:
                         self.nfa.add_state(str(previous_state))
@@ -72,34 +76,31 @@ class AutomataBuilder:
                     )
                     partial_sat = True
             # return after the for loop is finished.
-            if partial_sat:
-                if not self.create_all:
-                    return
+            if partial_sat and not self.create_all:
+                return
 
         # when the work_list is empty, building nfa is completed.
         self.__build_completed = True
 
-    # only use for z_neq != 0. When adding neq arithmetic to solver, it automatically converts to eq arithmetic and neq (z_neq != 0) arithmetic.
-    def neq_to_nfa(self) -> None:
-        if self.coefs.count(1) != 1 and self.coefs.count(0) != len(self.coefs) - 1:
-            raise ValueError("coefs must be containing one 1 and the rest as 0s.")
-        # extract the only element from the set. Raises ValueError if the set is has too many or too few elements.
-        # This nfa must have only one final state.
-        (final_state,) = self.nfa.final_states
-        for input_symbol in self.nfa.input_symbols:
-            if "0" in input_symbol:
-                self.nfa.add_transition(
-                    self.nfa.initial_state, input_symbol, self.nfa.initial_state
-                )
-                self.nfa.add_transition(final_state, input_symbol, final_state)
+    # TODO: Fix this method
+    # def neq_to_nfa(self) -> None:
+    #     # extract the only element from the set. Raises ValueError if the set is has too many or too few elements.
+    #     # This nfa must have only one final state.
+    #     (final_state,) = self.nfa.final_states
+    #     for input_symbol in self.nfa.input_symbols:
+    #         if "0" in input_symbol:
+    #             self.nfa.add_transition(
+    #                 self.nfa.initial_state, input_symbol, self.nfa.initial_state
+    #             )
+    #             self.nfa.add_transition(final_state, input_symbol, final_state)
 
-            else:  # '1' in input_symbol
-                self.nfa.add_transition(
-                    self.nfa.initial_state, input_symbol, final_state
-                )
-                self.nfa.add_transition(final_state, input_symbol, final_state)
+    #         else:  # '1' in input_symbol
+    #             self.nfa.add_transition(
+    #                 self.nfa.initial_state, input_symbol, final_state
+    #             )
+    #             self.nfa.add_transition(final_state, input_symbol, final_state)
 
-        self.__build_completed = True
+    #     self.__build_completed = True
 
     def leq_to_nfa(self) -> None:
         partial_sat = False
@@ -107,24 +108,23 @@ class AutomataBuilder:
         while self.work_list:
             current_state = self.work_list.pop()
             for symbol in self.nfa.input_symbols:
-                dot = dot_product_with_wildcard(self.coefs, symbol)
-                previous_state = int(
-                    (1 / 2 * (current_state - dot)) // 1
-                )  # calculate floor
+                dot = dot_product_with_wildcard(
+                    self.declared_vars_index_map, self.coefs, symbol
+                )
+                previous_state = (current_state - dot) // 2
                 if str(previous_state) not in self.nfa.states:
                     self.nfa.add_state(str(previous_state))
                     self.work_list.append(previous_state)
                 self.nfa.add_transition(str(previous_state), symbol, str(current_state))
 
-                if 1 / 2 * (current_state + dot) >= 0:
+                if current_state + dot >= 0:
                     self.nfa.add_transition(
                         self.INITIAL_STATE, symbol, str(current_state)
                     )
                     partial_sat = True
             # return after the for loop is finished.
-            if partial_sat:
-                if not self.create_all:
-                    return
+            if partial_sat and not self.create_all:
+                return
 
         # when the work_list is empty, building nfa is completed.
         self.__build_completed = True
