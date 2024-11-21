8fbdd0cdc3bccc6dbe4c490cf63037024f7d8f50
tanashou <tanashou0210@gmail.com>
2024-11-21 09:23:52 +0000

    add feature to convert not equals formula to union of less than and greater than

--- src/automata_based_smt_solver/parser/neq_converter.py
+++ src/automata_based_smt_solver/parser/neq_converter.py
@@ -1,13 +1,15 @@
-# ruff: noqa: ANN201, ANN204, ANN001, ANN003, ARG002
-from pysmt.walkers import DagWalker
-from pysmt.shortcuts import GT, LT
+# ruff: noqa: ANN201, ANN204, ANN001, ANN003, ARG002, D101, D107, D102
+from pysmt.shortcuts import GT, LT, Or
+from pysmt.walkers import IdentityDagWalker
 
 
-class NeqConverter(DagWalker):
+class NeqConverter(IdentityDagWalker):
     def __init__(self):
         super().__init__()
 
-    def walk_not_equals(self, formula, args, **kwargs):
-        print("not equals")
-        print(formula, args)
-        return args
+    def walk_not(self, formula, args, **kwargs):
+        # CNF に変換後に実行されるので、not は単一リテラルにのみ現れる。len(args) == 1
+        if args[0].is_equals():
+            lhs, rhs = args[0].args()
+            return Or(LT(lhs, rhs), GT(lhs, rhs))
+        return formula
