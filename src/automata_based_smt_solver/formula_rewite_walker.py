fe31533a6ef5b789b10c65b6d51fd8d382d846ee
tanashou <tanashou0210@gmail.com>
2024-11-23 10:28:18 +0000

    ruff: remove unused linting rules from formula_rewrite_walker.py

--- src/automata_based_smt_solver/formula_rewite_walker.py
+++ src/automata_based_smt_solver/formula_rewite_walker.py
@@ -1,4 +1,4 @@
-# ruff: noqa: ANN201, ANN204, ANN001, ANN003, ARG002, D101, D107, D102, ANN101
+# ruff: noqa: ANN201, ANN204, ANN001, ANN003, ARG002, D101, D107, D102
 from pysmt.fnode import FNode
 from pysmt.shortcuts import GE, GT, LT, Equals, Int, Or
 from pysmt.walkers import IdentityDagWalker
