d22c3d49a5592f2264ac51a377af7f2a82a797d5
tanashou <tanashou0210@gmail.com>
2024-11-25 08:33:52 +0000

    add InputSymbol class

--- src/automata_based_smt_solver/automata/input_symbol.py
+++ src/automata_based_smt_solver/automata/input_symbol.py
@@ -1,2 +1,15 @@
 class InputSymbol:
-    pass
+    def __init__(self, value: int) -> None:
+        self.value = value
+
+    def dot(self, vector: list[int]) -> int:
+        result = 0
+        value = self.value
+
+        for i, v in enumerate(reversed(vector)):
+            if value & (1 << i):
+                result += v
+        return result
+
+    def apply_mask(self, mask: int) -> int:
+        return self.value & mask
