e7fccf6ce92064a9668ea0621e8b8deeaac598c5
tanashou <tanashou0210@gmail.com>
2024-11-27 09:44:59 +0000

    refactor: enhance InputSymbol class with dataclass and error handling for epsilon symbol

--- src/automata_based_smt_solver/automata/input_symbol.py
+++ src/automata_based_smt_solver/automata/input_symbol.py
@@ -1,6 +1,10 @@
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
 class InputSymbol:
-    def __init__(self, value: int) -> None:
-        self.value = value
+    # None represents epsilon symbol
+    value: int | None
 
     def __hash__(self) -> int:
         return hash(self.value)
@@ -11,6 +15,15 @@ class InputSymbol:
         return False
 
     def dot(self, vector: list[int]) -> int:
+        if self.value is None:
+            msg = "Cannot calculate dot product with epsilon symbol"
+            raise ValueError(msg)
+
+        bit_length = self.value.bit_length()
+        if bit_length > len(vector):
+            msg = "Vector length is smaller than the number of bits in value"
+            raise ValueError(msg)
+
         result = 0
         value = self.value
 
@@ -20,4 +33,7 @@ class InputSymbol:
         return result
 
     def apply_mask(self, mask: int) -> int:
+        if self.value is None:
+            msg = "Cannot apply mask to epsilon symbol"
+            raise ValueError(msg)
         return self.value & mask
