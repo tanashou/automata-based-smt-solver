bb59920b716843a90fd5cf5004c10c18603b4cba
tanashou <tanashou0210@gmail.com>
2024-12-16 04:13:48 +0000

    add comment

--- src/automata_based_smt_solver/automata/state.py
+++ src/automata_based_smt_solver/automata/state.py
@@ -4,6 +4,7 @@ from uuid import UUID
 
 @dataclass(frozen=True)
 class State:
+    # 同じ状態でも区別するためにuuidを持つ。union で使う。
     state_value: str | int
     uuid: UUID = field(default_factory=lambda: UUID(int=0))
 
