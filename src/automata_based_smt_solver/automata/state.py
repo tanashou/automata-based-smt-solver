68d1a416019b24ace8e77b80d89d4edcb7c1e1a1
tanashou <tanashou0210@gmail.com>
2024-12-28 09:20:46 +0000

    refactor: change state_value type to Any for greater flexibility in State class

--- src/automata_based_smt_solver/automata/state.py
+++ src/automata_based_smt_solver/automata/state.py
@@ -1,11 +1,12 @@
 from dataclasses import dataclass, field
+from typing import Any
 from uuid import UUID
 
 
 @dataclass(frozen=True)
 class State:
     # 同じ状態でも区別するためにuuidを持つ。union で使う。
-    state_value: str | int
+    state_value: Any
     uuid: UUID = field(default_factory=lambda: UUID(int=0))
 
     def __str__(self) -> str:
