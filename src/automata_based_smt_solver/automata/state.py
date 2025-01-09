766cc15862ca314efcf261a7df3cdb19aceeb2db
tanashou <tanashou0210@gmail.com>
2025-01-09 07:51:32 +0000

    refactor State class to initialize ID with -1 for unassigned states

--- src/automata_based_smt_solver/automata/state.py
+++ src/automata_based_smt_solver/automata/state.py
@@ -6,7 +6,7 @@ from typing import Any
 class State:
     # 同じ状態でも区別するためにuuidを持つ。union で使う。
     state_value: Any
-    id: int = 0
+    id: int = -1
 
     # for better usage
     def __post_init__(self) -> None:
@@ -26,6 +26,9 @@ class State:
         # show_diagram では状態はここで区別される。初期状態は空白で表示する。
         if self.state_value == "":
             return " "
+        # intersection を取る際、id が不要。新しく作る状態に id を指定しない。
+        if self.id == -1:
+            return str(self.state_value)
         return f"{self.state_value}({self.id})"
 
     def __repr__(self) -> str:
