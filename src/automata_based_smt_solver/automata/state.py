b4771ddab71b2453ca9268969d1454a4446b8ccf
tanashou <tanashou0210@gmail.com>
2025-01-09 05:44:05 +0000

    refactor State class to use integer ID instead of UUID for uniqueness

--- src/automata_based_smt_solver/automata/state.py
+++ src/automata_based_smt_solver/automata/state.py
@@ -1,13 +1,12 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from typing import Any
-from uuid import UUID
 
 
 @dataclass(frozen=True)
 class State:
     # 同じ状態でも区別するためにuuidを持つ。union で使う。
     state_value: Any
-    uuid: UUID = field(default_factory=lambda: UUID(int=0))
+    id: int = 0
 
     # for better usage
     def __post_init__(self) -> None:
@@ -16,15 +15,18 @@ class State:
             raise TypeError(msg)
 
     def __hash__(self) -> int:
-        return hash((self.state_value, self.uuid))
+        return hash((self.state_value, self.id))
 
     def __eq__(self, other: object) -> bool:
         if isinstance(other, State):
-            return self.state_value == other.state_value and self.uuid == other.uuid
+            return self.state_value == other.state_value and self.id == other.id
         return False
 
     def __str__(self) -> str:
-        return str(self.state_value)
+        # show_diagram では状態はここで区別される。初期状態は空白で表示する。
+        if self.state_value == "":
+            return " "
+        return f"{self.state_value}({self.id})"
 
     def __repr__(self) -> str:
         return self.__str__()
