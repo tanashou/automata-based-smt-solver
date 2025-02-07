7b2f006f442363fd71f7bbc51afc4bcd7593c650
tanashou <tanashou0210@gmail.com>
2025-02-07 07:21:29 +0000

    add MinusEliminator class for transforming binary minus operations into addition

--- src/automata_based_smt_solver/formula/rewriter.py
+++ src/automata_based_smt_solver/formula/rewriter.py
@@ -1,14 +1,18 @@
 from pysmt.fnode import FNode
 from pysmt.rewritings import CNFizer
 
+from automata_based_smt_solver.formula.minus_eliminator import MinusEliminator
 from automata_based_smt_solver.formula.negation_eliminator import NegationEliminator
 
 
 class Rewriter:
     def __init__(self) -> None:
         self.cnfizer = CNFizer()
-        self.walker = NegationEliminator()
+        self.negation_eliminator = NegationEliminator()
+        self.minus_eliminator = MinusEliminator()
 
     def cnfize_and_rewrite(self, formula: FNode) -> FNode:
         cnf = self.cnfizer.convert_as_formula(formula)
-        return self.walker.walk(cnf)
+        # ドキュメントにwalker を一括で扱うクラスがあった。
+        negation_eliminated_cnf = self.negation_eliminator.walk(cnf)
+        return self.minus_eliminator.walk(negation_eliminated_cnf)
