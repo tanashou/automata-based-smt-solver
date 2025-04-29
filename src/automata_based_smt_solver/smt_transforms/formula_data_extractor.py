078cef952b5e0b724adb768d5cacadf44e9e9538
tanashou <tanashou0210@gmail.com>
2025-04-29 09:14:41 +0000

    rename directory to smt_transforms

--- src/automata_based_smt_solver/smt_transforms/formula_data_extractor.py
+++ src/automata_based_smt_solver/smt_transforms/formula_data_extractor.py
@@ -5,7 +5,7 @@ from pysmt.exceptions import UnsupportedOperatorError
 from pysmt.fnode import FNode
 from pysmt.walkers import DagWalker
 
-from automata_based_smt_solver.formula.formula_type import FormulaType
+from automata_based_smt_solver.smt_transforms.formula_type import FormulaType
 
 
 @dataclass
