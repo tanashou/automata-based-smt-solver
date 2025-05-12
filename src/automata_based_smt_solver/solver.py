789ef0036ec05986c11ca00a18790de47b8849b9
tanashou <tanashou0210@gmail.com>
2025-05-12 07:28:33 +0000

    refactor: replace MinusEliminator with BracketExpander in CNF processing

--- src/automata_based_smt_solver/solver.py
+++ src/automata_based_smt_solver/solver.py
@@ -1,11 +1,11 @@
 from pysmt.fnode import FNode
 from pysmt.rewritings import CNFizer
 
+from automata_based_smt_solver.smt_transforms.bracket_expander import BracketExpander
 from automata_based_smt_solver.smt_transforms.formula_data_extractor import (
     FormulaData,
     FormulaDataExtractor,
 )
-from automata_based_smt_solver.smt_transforms.minus_eliminator import MinusEliminator
 from automata_based_smt_solver.smt_transforms.negation_eliminator import (
     NegationEliminator,
 )
@@ -15,7 +15,7 @@ class Solver:
     def _cnfize_and_eliminate_minus_negation(self, formula: FNode) -> FNode:
         cnf = CNFizer().convert_as_formula(formula)
         negation_eliminated_cnf = NegationEliminator().walk(cnf)
-        return MinusEliminator().walk(negation_eliminated_cnf)
+        return BracketExpander().walk(negation_eliminated_cnf)
 
     def _extract_data(self, cnf: FNode) -> list[list[FormulaData]]:
         result = []
