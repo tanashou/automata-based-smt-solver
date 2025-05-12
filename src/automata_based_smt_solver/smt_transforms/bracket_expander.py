2fdf431c35879e5fd6b45d0efecaf59299290785
tanashou <tanashou0210@gmail.com>
2025-05-12 06:51:20 +0000

    refactor: improve walk_times and walk_minus methods for better handling of integer constants and argument distribution

--- src/automata_based_smt_solver/smt_transforms/bracket_expander.py
+++ src/automata_based_smt_solver/smt_transforms/bracket_expander.py
@@ -5,8 +5,6 @@ from pysmt.walkers import IdentityDagWalker
 
 
 class BracketExpander(IdentityDagWalker):
-    MINUS_ARITY = 2
-
     def __init__(self) -> None:
         super().__init__()
 
@@ -23,47 +21,68 @@ class BracketExpander(IdentityDagWalker):
         if int_sum != 0:
             flat_args.append(Int(int_sum))
         if len(flat_args) == 1:
-            # result is an integer
+            # result is an integer or a symbol
             return flat_args[0]
         return Plus(flat_args)
 
     def walk_times(self, formula: FNode, args: list[FNode], **_kwargs: object) -> FNode:
-        pass
-
-    def _flatten_times_args(self, args: list[FNode]) -> tuple[list[FNode], int, bool]:
         flat_args = []
         int_prod = 1
-        has_int = False
+
+        # 内側の括弧はすでに展開されているかも
+        for arg in args:
+            if arg.is_int_constant():
+                int_prod *= arg.constant_value()
+            else:
+                flat_args.append(arg)
+
+        if int_prod != 1:
+            flat_args.append(Int(int_prod))
+        if len(flat_args) == 1:
+            # result is an integer or a symbol
+            return flat_args[0]
+
+        # Distribute multiplication over addition if any argument is a Plus node
+        for i, arg in enumerate(flat_args):
+            if arg.is_plus():
+                # Distribute Times over Plus: a * (b + c) => a*b + a*c
+                others = flat_args[:i] + flat_args[i + 1 :]
+                distributed = [
+                    self.walk_times(formula, [term, *others]) for term in arg.args()
+                ]
+                return self.walk_plus(Plus(distributed), distributed)
+
+        # Flatten nested Times and combine integer constants
+        flat_args, int_prod = self._flatten_times_args(flat_args)
+        if int_prod != 1:
+            flat_args.append(Int(int_prod))
+        if len(flat_args) == 1:
+            return flat_args[0]
+        return Times(flat_args)
+
+    def _flatten_times_args(self, args: list[FNode]) -> tuple[list[FNode], int]:
+        flat_args = []
+        int_prod = 1
         for arg in args:
             if arg.is_times():
                 for sub in arg.args():
                     if sub.is_int_constant():
                         int_prod *= sub.constant_value()
-                        has_int = True
                     else:
                         flat_args.append(sub)
             elif arg.is_int_constant():
                 int_prod *= arg.constant_value()
-                has_int = True
             else:
                 flat_args.append(arg)
-        return flat_args, int_prod, has_int
+        return flat_args, int_prod
 
     def walk_minus(self, formula: FNode, args: list[FNode], **_kwargs: object) -> FNode:
         # args length is guaranteed to be 2
-        minuend, subtrahend = args
+        left, right = args
 
-        term = Times(Int(-1), subtrahend)
-        rewritten_term = self.walk_times(term, list(term.args()))
-        return Plus(minuend, rewritten_term)
-
-    def walk_par(self, _formula: FNode, args: list[FNode], **_kwargs: object) -> FNode:
-        if len(args) != 1:
-            msg = "Par node should have exactly one child"
-            raise ValueError(msg)
-        return args[0]
-
-    def walk_default(
-        self, formula: FNode, args: list[FNode], **kwargs: object
-    ) -> FNode:
-        return formula.__class__(*args)
+        negated_right = Times(Int(-1), right)
+        negated_right_expanded = self.walk_times(
+            negated_right, list(negated_right.args())
+        )
+        sum_expr = Plus(left, negated_right_expanded)
+        return self.walk_plus(sum_expr, list(sum_expr.args()))
