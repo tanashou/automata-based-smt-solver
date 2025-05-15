49531e65de2d13a3ed1ede30f4be8a7a6a8ae05f
tanashou <tanashou0210@gmail.com>
2025-05-15 06:36:39 +0000

    refactor: update formula rewriting process to use TimesDistributor and improve clarity

--- src/automata_based_smt_solver/solver.py
+++ src/automata_based_smt_solver/solver.py
@@ -1,8 +1,11 @@
+import pysmt.rewritings
 from pysmt.fnode import FNode
-from pysmt.rewritings import CNFizer
+from pysmt.rewritings import TimesDistributor
 from pysmt.smtlib.parser import SmtLibParser
 
-from automata_based_smt_solver.smt_transforms.bracket_expander import BracketExpander
+from automata_based_smt_solver.smt_transforms.calculating_bracket_expander import (
+    CalculatingBracketExpander,
+)
 from automata_based_smt_solver.smt_transforms.formula_data_extractor import (
     FormulaData,
     FormulaDataExtractor,
@@ -10,22 +13,25 @@ from automata_based_smt_solver.smt_transforms.formula_data_extractor import (
 from automata_based_smt_solver.smt_transforms.negation_eliminator import (
     NegationEliminator,
 )
+from automata_based_smt_solver.smt_transforms.smtlib_reader import SMTLIBReader
 
 
 class Solver:
     def __init__(self) -> None:
         self.parser = SmtLibParser()
+        self.reader = SMTLIBReader()
 
-    def _cnfize_and_eliminate_minus_negation(self, formula: FNode) -> FNode:
-        cnf = CNFizer().convert_as_formula(formula)
+    def _rewrite_formula(self, formula: FNode) -> FNode:
+        cnf = pysmt.rewritings.cnf(formula)
         negation_eliminated_cnf = NegationEliminator().walk(cnf)
-        return BracketExpander().walk(negation_eliminated_cnf)
+        distributed = TimesDistributor().walk(negation_eliminated_cnf)
+        return CalculatingBracketExpander().walk(distributed)
 
     def _extract_data(self, cnf: FNode) -> list[list[FormulaData]]:
         result = []
         data_extractor = FormulaDataExtractor()
-        for clause in cnf:
-            literals = [data_extractor.extract(literal) for literal in clause]
+        for clause in cnf.args():
+            literals = [data_extractor.extract(literal) for literal in clause.args()]
             result.append(literals)
 
         return result
