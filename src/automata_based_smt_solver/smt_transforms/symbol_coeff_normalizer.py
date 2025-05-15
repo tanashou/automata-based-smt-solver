79a954787fa56180a6bf5fcb219d041d7d046d70
tanashou <tanashou0210@gmail.com>
2025-05-15 07:57:40 +0000

    fix: handle BOOL type in walk_symbol method of SymbolCoeffNormalizer

--- src/automata_based_smt_solver/smt_transforms/symbol_coeff_normalizer.py
+++ src/automata_based_smt_solver/smt_transforms/symbol_coeff_normalizer.py
@@ -1,6 +1,7 @@
 # ruff: noqa: ANN201, ANN204, ANN001, ANN003, ARG002, D101, D107, D102
 
 from pysmt.shortcuts import Int, Times
+from pysmt.typing import BOOL
 from pysmt.walkers import IdentityDagWalker
 
 
@@ -9,4 +10,6 @@ class SymbolCoeffNormalizer(IdentityDagWalker):
         super().__init__()
 
     def walk_symbol(self, formula, args, **kwargs):
+        if formula.get_type() == BOOL:
+            return formula
         return Times(Int(1), formula)
