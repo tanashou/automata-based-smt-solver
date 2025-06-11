777aeec2b0023faad2a7889655c5e92f2e4798ea
tanashou <tanashou0210@gmail.com>
2025-06-11 06:49:52 +0000

    feat: add DNFGenerator to module exports for improved accessibility

--- src/automata_based_smt_solver/formula/rewritings/__init__.py
+++ src/automata_based_smt_solver/formula/rewritings/__init__.py
@@ -1,10 +1,12 @@
 from .calculating_bracket_expander import CalculatingBracketExpander
+from .dnf_generator import DNFGenerator
 from .negation_eliminator import NegationEliminator
 from .or_flattener import OrFlattener
 from .symbol_coeff_normalizer import SymbolCoeffNormalizer
 
 __all__ = [
     "CalculatingBracketExpander",
+    "DNFGenerator",
     "NegationEliminator",
     "OrFlattener",
     "SymbolCoeffNormalizer",
