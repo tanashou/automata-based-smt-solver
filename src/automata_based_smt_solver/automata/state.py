4fcd96c0eedbe7b41766fd7785033ac6c7010178
tanashou <tanashou0210@gmail.com>
2025-06-12 04:46:30 +0000

    feat: implement __hash__ and __eq__ methods in State class for improved functionality

--- src/automata_based_smt_solver/automata/state.py
+++ src/automata_based_smt_solver/automata/state.py
@@ -8,3 +8,11 @@ NFAStateT: TypeAlias = str | int | tuple["NFAStateT", ...]
 class State:
     value: NFAStateT
     has_path_to_final: bool = False
+
+    def __hash__(self) -> int:
+        return hash(self.value)
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, State):
+            return False
+        return self.value == other.value
