4147711cdeebf6451300fafa6016ecd058ae57cb
tanashou <tanashou0210@gmail.com>
2025-06-26 08:07:43 +0000

    refactor: update NFA class to use NFAStateT type alias for states and transitions

--- src/automata_based_smt_solver/automata/nfa.py
+++ src/automata_based_smt_solver/automata/nfa.py
@@ -14,33 +14,33 @@ from automata_based_smt_solver.automata.msbf_alphabet import MSBFAlphabet
 from automata_based_smt_solver.automata.msbf_alphabet_symbol import (
     MSBFAlphabetSymbol,
 )
-from automata_based_smt_solver.automata.state import NFAStateT, State
 
 if TYPE_CHECKING:
     from collections.abc import Iterable
 
-NFATransitionsT: TypeAlias = dict[State, dict[MSBFAlphabetSymbol, set[State]]]
+NFAStateT: TypeAlias = str | int | tuple["NFAStateT", ...]
+NFATransitionsT: TypeAlias = dict[NFAStateT, dict[MSBFAlphabetSymbol, set[NFAStateT]]]
 
 
 class NFA:
     """NFA represents a custom NFA for use in the automata-based SMT solver.
 
     Attributes:
-        _states (set[State]): Set of all states.
+        _states (set[NFAStateT]): Set of all states.
         _input_symbols (set[MSBFAlphabetSymbol]): Set of input symbols.
         _transitions (NFATransitionsT): Transition mapping.
-        _initial_state (State): Starting state.
-        _final_states (set[State]): Set of accepting states.
+        _initial_state (NFAStateT): Starting state.
+        _final_states (set[NFAStateT]): Set of accepting states.
 
     """
 
     def __init__(
         self,
-        states: set[State],
+        states: set[NFAStateT],
         input_symbols: MSBFAlphabet,
         transitions: NFATransitionsT,
-        initial_state: State,
-        final_states: set[State],
+        initial_state: NFAStateT,
+        final_states: set[NFAStateT],
     ) -> None:
         """Initialize a NFA."""
         self._states = states
@@ -68,7 +68,7 @@ class NFA:
         return f"NFA({self})"
 
     @property
-    def states(self) -> set[State]:
+    def states(self) -> set[NFAStateT]:
         return self._states
 
     @property
@@ -80,25 +80,25 @@ class NFA:
         return self._transitions
 
     @property
-    def initial_state(self) -> State:
+    def initial_state(self) -> NFAStateT:
         return self._initial_state
 
     @property
-    def final_states(self) -> set[State]:
+    def final_states(self) -> set[NFAStateT]:
         return self._final_states
 
-    def add_state(self, new_state: State) -> None:
+    def add_state(self, new_state: NFAStateT) -> None:
         self._states.add(new_state)
 
-    def add_states(self, states: set[State]) -> None:
+    def add_states(self, states: set[NFAStateT]) -> None:
         self._states.update(states)
 
     def _set_initial_state(self) -> None:
-        initial_state_name = "q0"
-        self._initial_state = State(value=initial_state_name)
+        initial_state = "q0"
+        self._initial_state = initial_state
         self._states.add(self._initial_state)
 
-    def _set_custom_initial_state(self, initial_state: State) -> None:
+    def _set_custom_initial_state(self, initial_state: NFAStateT) -> None:
         """Set a custom initial state for the NFA. Only used in intersection."""
         self._states.remove(self._initial_state)
         self._initial_state = initial_state
@@ -106,24 +106,24 @@ class NFA:
 
     def add_transition(
         self,
-        start_state: State,
+        start_state: NFAStateT,
         symbol: MSBFAlphabetSymbol,
-        end_state: State,
+        end_state: NFAStateT,
     ) -> None:
         self._transitions[start_state][symbol].add(end_state)
 
     def set_transitions(self, transitions: NFATransitionsT) -> None:
         self._transitions = transitions
 
-    def add_final_state(self, new_final_state: State) -> None:
+    def add_final_state(self, new_final_state: NFAStateT) -> None:
         self._final_states.add(new_final_state)
 
-    def set_final_states(self, final_states: set[State]) -> None:
+    def set_final_states(self, final_states: set[NFAStateT]) -> None:
         self._final_states = final_states
 
     def get_next_states(
-        self, current_state: State, input_symbol: MSBFAlphabetSymbol
-    ) -> set[State]:
+        self, current_state: NFAStateT, input_symbol: MSBFAlphabetSymbol
+    ) -> set[NFAStateT]:
         """Get states reachable from current_state via input_symbol with wildcards.
 
         Args:
@@ -131,7 +131,7 @@ class NFA:
             input_symbol: Input symbol to process (may contain wildcards).
 
         Returns:
-            set[State]: Set of states reachable via the input symbol.
+            set[NFAStateT]: Set of states reachable via the input symbol.
 
         """
         # Get all transitions from the current state
@@ -205,16 +205,16 @@ class NFA:
         raise NotImplementedError
 
     def intersection(self, other: "NFA") -> "NFA":  # noqa: C901
-        new_states: set[State] = set()
+        new_states: set[NFAStateT] = set()
         new_input_symbols: MSBFAlphabet = MSBFAlphabet.union_alphabet(
             self.input_symbols, other.input_symbols
         )
         new_transitions: NFATransitionsT = defaultdict(lambda: defaultdict(set))
         new_initial_state_value: tuple[NFAStateT, NFAStateT] = (
-            self.initial_state.value,
-            other.initial_state.value,
+            self.initial_state,
+            other.initial_state,
         )
-        new_states.add(State(new_initial_state_value))
+        new_states.add(new_initial_state_value)
 
         queue: deque[tuple[NFAStateT, NFAStateT]] = deque()
         queue.append(new_initial_state_value)
@@ -225,27 +225,27 @@ class NFA:
         while queue:
             curr_state_value = queue.popleft()
             q_a, q_b = curr_state_value
-            next_states_iterables: list[Iterable[State]] = []
+            next_states_iterables: list[Iterable[NFAStateT]] = []
 
-            transitions_a = self.transitions.get(State(q_a), {})
-            transitions_b = other.transitions.get(State(q_b), {})
+            transitions_a = self.transitions.get(q_a, {})
+            transitions_b = other.transitions.get(q_b, {})
 
             # Add all transitions moving over same input symbols
             for symbol in new_input_symbols.symbol_generator():
-                end_states_a: set[State] = set()
+                end_states_a: set[NFAStateT] = set()
                 for key, dests in transitions_a.items():
                     if symbol == key:
                         end_states_a.update(dests)
 
-                end_states_b: set[State] = set()
+                end_states_b: set[NFAStateT] = set()
                 for key, dests in transitions_b.items():
                     if symbol == key:
                         end_states_b.update(dests)
 
                 if end_states_a and end_states_b:
-                    state_dict = new_transitions[State(curr_state_value)]
+                    state_dict = new_transitions[curr_state_value]
                     product_states = [
-                        State((state_a.value, state_b.value))
+                        (state_a, state_b)
                         for state_a in end_states_a
                         for state_b in end_states_b
                     ]
@@ -256,22 +256,20 @@ class NFA:
                 if product_state not in new_states:
                     new_states.add(product_state)
                     if (
-                        isinstance(product_state.value, tuple)
-                        and len(product_state.value) == intersection_tuple_len
+                        isinstance(product_state, tuple)
+                        and len(product_state) == intersection_tuple_len
                     ):
-                        queue.append(product_state.value)
+                        queue.append(product_state)
 
-        new_final_states: set[State] = {
-            State((q_a.value, q_b.value))
-            for q_a in self.final_states
-            for q_b in other.final_states
+        new_final_states: set[NFAStateT] = {
+            (q_a, q_b) for q_a in self.final_states for q_b in other.final_states
         }
 
         return self.__class__(
             states=new_states,
             input_symbols=new_input_symbols,
             transitions=new_transitions,
-            initial_state=State(new_initial_state_value),
+            initial_state=new_initial_state_value,
             final_states=new_final_states,
         )
 
@@ -286,7 +284,7 @@ class NFA:
         """Perform incremental intersection of two NFAs with changes."""
         intersection_tuple_len = 2
         intersected_nfa_new = copy.deepcopy(intersected_nfa_old)
-        work_list: deque[tuple[State, MSBFAlphabetSymbol, State]] = deque()
+        work_list: deque[tuple[NFAStateT, MSBFAlphabetSymbol, NFAStateT]] = deque()
 
         # seed N1 changes
         for q1_from, transitions_from_q1 in delta_1_changes.items():
@@ -298,10 +296,8 @@ class NFA:
                             and symbol in n2_new.transitions[q2]
                         ):
                             for q2_to in n2_new.transitions[q2][symbol]:
-                                intersected_state_from = State(
-                                    (q1_from.value, q2.value)
-                                )
-                                intersected_state_to = State((q1_to.value, q2_to.value))
+                                intersected_state_from = (q1_from, q2)
+                                intersected_state_to = (q1_to, q2_to)
                                 work_list.append(
                                     (
                                         intersected_state_from,
@@ -320,10 +316,8 @@ class NFA:
                             and symbol in n1_new.transitions[q1]
                         ):
                             for q1_to in n1_new.transitions[q1][symbol]:
-                                intersected_state_from = State(
-                                    (q1.value, q2_from.value)
-                                )
-                                intersected_state_to = State((q1_to.value, q2_to.value))
+                                intersected_state_from = (q1, q2_from)
+                                intersected_state_to = (q1_to, q2_to)
                                 work_list.append(
                                     (
                                         intersected_state_from,
@@ -359,7 +353,7 @@ class NFA:
                 intersected_nfa_new.states.add(intersected_state_to)
 
                 # Check if it's an accepting state
-                value = intersected_state_to.value
+                value = intersected_state_to
                 if isinstance(value, tuple) and len(value) == intersection_tuple_len:
                     r1, r2 = value
                 else:
@@ -372,14 +366,14 @@ class NFA:
                 # Propagate changes
                 for next_symbol in n1_new.input_symbols.symbol_generator():
                     if (
-                        State(r1) in n1_new.transitions
-                        and next_symbol in n1_new.transitions[State(r1)]
-                        and State(r2) in n2_new.transitions
-                        and next_symbol in n2_new.transitions[State(r2)]
+                        r1 in n1_new.transitions
+                        and next_symbol in n1_new.transitions[r1]
+                        and r2 in n2_new.transitions
+                        and next_symbol in n2_new.transitions[r2]
                     ):
-                        for s1 in n1_new.transitions[State(r1)][next_symbol]:
-                            for s2 in n2_new.transitions[State(r2)][next_symbol]:
-                                next_intersected_state = State((s1.value, s2.value))
+                        for s1 in n1_new.transitions[r1][next_symbol]:
+                            for s2 in n2_new.transitions[r2][next_symbol]:
+                                next_intersected_state = (s1, s2)
                                 work_list.append(
                                     (
                                         intersected_state_to,
