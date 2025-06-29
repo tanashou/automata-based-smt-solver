c37e805417876a63b318a2df7cb8938f263679ca
tanashou <tanashou0210@gmail.com>
2025-06-29 06:46:42 +0000

    refactor: streamline post-create commands and add setup script for environment configuration

---
+++ .devcontainer/setup_uv.sh
@@ -0,0 +1,5 @@
+uv pip sync pyproject.toml
+# fixes ModuleNotFoundError: No module named '_distutils_hack' in python >3.12
+uv pip install setuptools
+uv pip install --group dev-dependencies -r pyproject.toml
+pre-commit install
