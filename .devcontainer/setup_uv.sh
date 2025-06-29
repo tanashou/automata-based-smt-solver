e843b630d2fb06a559aafac9eb1dd00c9775829e
tanashou <tanashou0210@gmail.com>
2025-06-29 07:25:30 +0000

    fix: add missing installation command for editable package in setup_uv.sh

--- .devcontainer/setup_uv.sh
+++ .devcontainer/setup_uv.sh
@@ -2,4 +2,5 @@ uv pip sync pyproject.toml
 # fixes ModuleNotFoundError: No module named '_distutils_hack' in python >3.12
 uv pip install setuptools
 uv pip install --group dev-dependencies -r pyproject.toml
+uv pip install -e .
 pre-commit install
