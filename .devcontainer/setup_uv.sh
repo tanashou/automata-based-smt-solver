6b1862bcab4653f4a09968517af6a120d21fc4d6
tanashou <tanashou0210@gmail.com>
2025-06-30 10:36:46 +0000

    fix: add comment to clarify editable installs limitation in setup_uv.sh

--- .devcontainer/setup_uv.sh
+++ .devcontainer/setup_uv.sh
@@ -2,5 +2,6 @@ uv pip sync pyproject.toml
 # fixes ModuleNotFoundError: No module named '_distutils_hack' in python >3.12
 uv pip install setuptools
 uv pip install --group dev-dependencies -r pyproject.toml
+# pyproject.toml don't support editable installs in env not managed by uv.
 uv pip install -e .
 pre-commit install
