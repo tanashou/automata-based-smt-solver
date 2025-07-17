7676ce111af863c4f52694bb8fc88b71857629f9
tanashou <tanashou0210@gmail.com>
2025-07-17 04:36:50 +0000

    feat: implement UniversalQFEliminator and add corresponding tests

--- src/absmt/formula/rewritings/__init__.py
+++ src/absmt/formula/rewritings/__init__.py
@@ -3,6 +3,7 @@ from .dnf_generator import DNFGenerator
 from .negation_eliminator import NegationEliminator
 from .or_flattener import OrFlattener
 from .symbol_coeff_normalizer import SymbolCoeffNormalizer
+from .universal_qf_eliminator import UniversalQFEliminator
 
 __all__ = [
     "CalculatingBracketExpander",
@@ -10,4 +11,5 @@ __all__ = [
     "NegationEliminator",
     "OrFlattener",
     "SymbolCoeffNormalizer",
+    "UniversalQFEliminator",
 ]
