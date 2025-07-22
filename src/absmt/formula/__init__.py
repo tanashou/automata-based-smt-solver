d84f2fad947c28a8065b31c5c52a8d740643ed3d
tanashou <tanashou0210@gmail.com>
2025-07-22 06:14:56 +0000

    refactor: remove FormulaDataExtractorQF and related tests, consolidating functionality into FormulaDataExtractor

--- src/absmt/formula/__init__.py
+++ src/absmt/formula/__init__.py
@@ -1,9 +1,7 @@
 from .formula_data_extractor import FormulaDataExtractor
-from .formula_data_extractor_qf import FormulaDataExtractorQF
 from .polynomial_normalizer import PolynomialNormalizer
 
 __all__ = [
     "FormulaDataExtractor",
-    "FormulaDataExtractorQF",
     "PolynomialNormalizer",
 ]
--- src/absmt/formula/formula_data_extractor.py
+++ src/absmt/formula/formula_data_extractor.py
@@ -1,102 +1,142 @@
-# ruff: noqa: ANN201, ANN001, ANN003, ARG002
-from collections import defaultdict
-
-from pysmt.exceptions import UnsupportedOperatorError
+# ruff: noqa: ANN201, ANN204, ANN001
 from pysmt.fnode import FNode
-from pysmt.walkers import DagWalker
+from pysmt.operators import AND, EQUALS, EXISTS, LE, OR
+from pysmt.shortcuts import Minus
 
-from absmt.formula.type import FormulaData, FormulaType, QuantifierType
+from absmt.formula.type import FormulaData, FormulaNodeType, FormulaType, QuantifierType
+
+from .polynomial_normalizer import PolynomialNormalizer
 
 
-class FormulaDataExtractor(DagWalker):
-    def __init__(self) -> None:
-        super().__init__(invalidate_memoization=True)
+class FormulaDataExtractor:
+    """A simple recursive walker to perform top-down context propagation."""
 
-    def extract(self, formula) -> list[list[FormulaData]]:  # [] 内は and, [] 同士が or
-        self._coeffs: defaultdict[str, int] = defaultdict(int)
-        self._const: int = 0
-        self._side_sign: int = 1
+    def __init__(self, env=None):
+        self.normalizer = PolynomialNormalizer(env=env)
+        self.memoization = {}
+        # Map node types from pysmt.operators to walk methods.
+        self.functions = {
+            EXISTS: self.walk_exists,
+            AND: self.walk_and,
+            OR: self.walk_or,
+            LE: self.walk_le,
+            EQUALS: self.walk_equals,
+            # Assumes that LT and FORALL have been eliminated in a pre-processing step.
+        }
 
-        if formula.is_equals():
-            self._formula_type = FormulaType.EQ
-        elif formula.is_le():
-            self._formula_type = FormulaType.LE
-        elif formula.is_lt():
-            self._formula_type = FormulaType.LE
-            self._const -= 1
-        else:
-            msg = "Unsupported formula type. Only EQ, LE, and LT are supported."
-            raise UnsupportedOperatorError(msg)
+    def extract_data(self, formula: FNode) -> object:
+        initial_context = {
+            "quantifier_type": QuantifierType.NONE,
+            "quantifier_vars": set(),
+        }
+        return self._walk(formula, initial_context)
 
-        # 括弧展開済み、定数は右辺か左辺に単体として、または足し算、引き算にしか現れない
-        lhs, rhs = formula.args()
-        if lhs.is_int_constant():
-            self._const -= lhs.constant_value()
-        if rhs.is_int_constant():
-            self._const += rhs.constant_value()
+    def _get_key(self, formula, context) -> tuple:
+        context_tuple = frozenset(
+            (
+                k,
+                frozenset(v) if isinstance(v, set) else v,
+            )  # Convert set to frozenset to make it hashable.
+            for k, v in sorted(context.items())
+        )
+        return (formula, context_tuple)
 
-        self._side_sign = 1
-        self.walk(lhs)
-        self._side_sign = -1
-        self.walk(rhs)
+    def _walk(self, formula, context) -> object:
+        key = self._get_key(formula, context)
+        if key in self.memoization:
+            return self.memoization[key]
 
-        final_coeffs = {k: v for k, v in self._coeffs.items() if v != 0}
+        # Use the FNode type (int) directly as the key.
+        node_type = formula.node_type()
+        func = self.functions.get(node_type)
+
+        result = func(formula, context) if func else None
+        self.memoization[key] = result
+        return result
+
+    def _get_normalized_data(self, formula, context) -> FormulaData:
+        left, right = formula.arg(0), formula.arg(1)
+        expr_to_normalize = Minus(left, right)
+        coeffs, const = self.normalizer.walk(expr_to_normalize)
+        final_const = -const
+        formula_type = FormulaType.EQ if formula.is_equals() else FormulaType.LE
         return FormulaData(
-            QuantifierType.NONE,
-            final_coeffs,
-            self._const,
-            self._formula_type,
-            self._has_negation_before_bool_var,
+            quantifier_type=context["quantifier_type"],
+            quantifier_vars=context["quantifier_vars"],
+            coeffs=coeffs,
+            const=final_const,
+            formula_type=formula_type,
         )
 
-    # Walker methods
+    def walk_exists(self, formula, context):
+        q_vars = {v.symbol_name() for v in formula.quantifier_vars()}
+        new_context = context.copy()
+        new_context["quantifier_type"] = QuantifierType.EXISTS
+        new_context["quantifier_vars"] = q_vars | context.get("quantifier_vars", set())
 
-    def walk_not(self, formula, args, **kwargs):
-        self._has_negation_before_bool_var = True
-        return formula
+        # Process the child node with the new context.
+        child_result = self._walk(formula.arg(0), new_context)
 
-    def walk_times(self, formula, args: list[FNode], **kwargs):
-        expected_arg_count = 2
-        if len(args) != expected_arg_count:
-            # var with 2 or more degrees exists"
-            msg = "TIMES operator must have exactly two arguments."
-            raise UnsupportedOperatorError(msg)
+        # Wrap the result in a list if it's not already a list.
+        if isinstance(child_result, list):
+            return child_result
+        if child_result is not None:
+            return [child_result]
+        return []
 
-        a, b = args
-        if a.is_int_constant() and b.is_symbol():
-            coeff = a.constant_value() * self._side_sign
-            self._coeffs[str(b)] += coeff
-        elif a.is_symbol() and b.is_int_constant():
-            coeff = b.constant_value() * self._side_sign
-            self._coeffs[str(a)] += coeff
+    def walk_and(self, formula, context):
+        args = [self._walk(arg, context) for arg in formula.args()]
+        return {
+            "type": FormulaNodeType.AND,
+            "args": [arg for arg in args if arg is not None],
+        }
+
+    def walk_or(self, formula, context):
+        args = [self._walk(arg, context) for arg in formula.args()]
+        return {
+            "type": FormulaNodeType.OR,
+            "args": [arg for arg in args if arg is not None],
+        }
+
+    def walk_le(self, formula, context):
+        return [self._get_normalized_data(formula, context)]
+
+    def walk_equals(self, formula, context):
+        if formula.arg(0).get_type().is_bool_type():
+            return None  # Iff is not supported.
+        return [self._get_normalized_data(formula, context)]
+
+
+def collect_literals_from_tree(tree_result: object) -> list[FormulaData]:
+    """Recursively collects all FormulaData objects (literals).
+
+    Args:
+        tree_result: The return value of extractor.extract_data().
+
+    Returns:
+        A set of all unique FormulaData objects contained in the formula.
+
+    """
+    literals = []
+    _recursive_collect(tree_result, literals)
+    return literals
+
+
+def _recursive_collect(node: object, literals: list[FormulaData]) -> None:
+    """Perform the recursive collection of FormulaData objects."""
+    if isinstance(node, dict):
+        # For AND/OR nodes, recurse on the list of children.
+        for child_node in node.get("args", []):
+            _recursive_collect(child_node, literals)
+
+    elif isinstance(node, list):
+        # If the object is a list.
+        if len(node) == 1 and isinstance(node[0], FormulaData):
+            # If the list contains a single FormulaData object, it's a literal (leaf).
+            literals.append(node[0])
         else:
-            msg = "Multiplication of variables (var * var) is not allowed in LIA."
-            raise UnsupportedOperatorError(msg)
-        return formula
+            # Otherwise, it's an intermediate list; recurse on each item.
+            for item in node:
+                _recursive_collect(item, literals)
 
-    def walk_plus(self, formula, args: list[FNode], **kwargs):
-        for arg in args:
-            if arg.is_int_constant():
-                self._const += arg.constant_value() * (-self._side_sign)
-        return formula
-
-    def walk_minus(self, formula, args, **kwargs):
-        # Minus is removed by pysmt.rewriter.TimesDistributor
-        return formula
-
-    def walk_and(self, formula, args, **kwargs):
-        msg = (
-            "Logical 'and' is not allowed. Please eliminate it before using this "
-            "extractor."
-        )
-        raise UnsupportedOperatorError(msg)
-
-    def walk_or(self, formula, args, **kwargs):
-        msg = (
-            "Logical 'or' is not allowed. Please eliminate it before using this "
-            "extractor."
-        )
-        raise UnsupportedOperatorError(msg)
-
-    def walk_int_constant(self, formula, args, **kwargs):
-        return formula
+    # Do nothing for FormulaData objects themselves or None.
