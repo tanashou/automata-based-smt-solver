c31a3f9833276f0ec0e5b4a9e99949be77038c2f
tanashou <tanashou0210@gmail.com>
2025-07-24 09:27:08 +0000

    feat: add DoubleNegationEliminator to module exports

--- src/absmt/formula/rewritings/__init__.py
+++ src/absmt/formula/rewritings/__init__.py
@@ -1,5 +1,6 @@
 from .calculating_bracket_expander import CalculatingBracketExpander
 from .dnf_generator import DNFGenerator
+from .double_negation_eliminator import DoubleNegationEliminator
 from .negation_eliminator import NegationEliminator
 from .or_flattener import OrFlattener
 from .quantifier_preserving_nnfizer import QuantifierPreservingNNFizer
@@ -9,6 +10,7 @@ from .universal_qf_eliminator import UniversalQFEliminator
 __all__ = [
     "CalculatingBracketExpander",
     "DNFGenerator",
+    "DoubleNegationEliminator",
     "NegationEliminator",
     "OrFlattener",
     "QuantifierPreservingNNFizer",
