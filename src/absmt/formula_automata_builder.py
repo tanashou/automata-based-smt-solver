ed2804d428633864aa0b70805b9155830b9ccb76
tanashou <tanashou0210@gmail.com>
2025-08-12 02:47:38 +0000

    feat: update FormulaAutomataBuilder to use SpotNFA and enhance build method

--- src/absmt/formula_automata_builder.py
+++ src/absmt/formula_automata_builder.py
@@ -1,14 +1,51 @@
+# ruff: noqa: ANN001, ANN003, ARG002, EM101
+import pysmt.operators as op
+import spot
 from pysmt.fnode import FNode
 from pysmt.walkers import DagWalker
+from pysmt.walkers.generic import handles
 
-from absmt.automata.nfa import NFA
+from absmt.automata.spot_nfa import SpotNFA
+from absmt.automata_builder import AutomataBuilder
+from absmt.formula import LiteralDataExtractor
 
 
 class FormulaAutomataBuilder(DagWalker):
     def __init__(self) -> None:
-        super().__init__(invalidate_memoization=True)
+        super().__init__()
+        self._literal_data_extractor = LiteralDataExtractor()
+        self._bdict = spot.make_bdd_dict()
 
-    def build(self, formula: FNode) -> NFA:
-        return self.walk(formula)
+    def build(self, formula: FNode) -> SpotNFA:
+        all_vars = [str(var) for var in formula.get_free_variables()]
+        all_vars += [str(var) for var in formula.quantifier_vars()]
+        all_var_index_map = {var: index for index, var in enumerate(all_vars)}
+        walk_context = {
+            "all_vars": all_vars,
+            "all_var_index_map": all_var_index_map,
+        }
+        return self.walk(formula, **walk_context)
 
     # TODO: Implement walker methods
+
+    def walk_exists(self, formula: FNode, args: list[SpotNFA], **kwargs) -> SpotNFA:
+        raise NotImplementedError("EXISTS operator handling is not implemented yet.")
+
+    def walk_and(self, formula: FNode, args: list[SpotNFA], **kwargs) -> SpotNFA:
+        raise NotImplementedError("AND operator handling is not implemented yet.")
+
+    def walk_or(self, formula: FNode, args: list[SpotNFA], **kwargs) -> SpotNFA:
+        raise NotImplementedError("OR operator handling is not implemented yet.")
+
+    def walk_not(self, formula: FNode, args: list[SpotNFA], **kwargs) -> SpotNFA:
+        raise NotImplementedError("NOT operator handling is not implemented yet.")
+
+    @handles(op.LE, op.EQUALS)
+    def walk_literal(self, formula: FNode, args, **kwargs) -> SpotNFA:
+        literal_data = self._literal_data_extractor.extract(formula)
+        all_vars = kwargs["all_vars"]
+        all_var_index_map = kwargs["all_var_index_map"]
+
+        builder = AutomataBuilder(literal_data, all_vars, all_var_index_map)
+        nfa = builder.build()
+        return SpotNFA(nfa, self._bdict)
