6f5a8281a7d4c84698681a7b3d27a260cbbc57e9
tanashou <81846890+tanashou@users.noreply.github.com>
2025-12-04 06:11:30 +0000

    recreate dnf generator to dnf converter. generator was causing bugs

--- src/absmt/formula/rewritings/__init__.py
+++ src/absmt/formula/rewritings/__init__.py
@@ -1,5 +1,5 @@
 from .calculating_bracket_expander import CalculatingBracketExpander
-from .dnf_generator import DNFGenerator
+from .dnf_converter import DNFConverter
 from .double_negation_eliminator import DoubleNegationEliminator
 from .negation_eliminator import NegationEliminator
 from .or_flattener import OrFlattener
@@ -9,7 +9,7 @@ from .universal_qf_eliminator import UniversalQFEliminator
 
 __all__ = [
     "CalculatingBracketExpander",
-    "DNFGenerator",
+    "DNFConverter",
     "DoubleNegationEliminator",
     "NegationEliminator",
     "OrFlattener",
